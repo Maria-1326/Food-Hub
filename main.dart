8396bc83f084a575832c7b5cdb456634bdafbd15
Maria <mariaarshad993@gmail.com>
2024-12-19 15:49:16 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_x/scr/features/authentication/screens/splash/splash_screen.dart';
+import 'package:get_x/scr/utils/theme/theme.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:splash_screens/screens/splash_screen.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -11,15 +13,12 @@ void main() => runApp(
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return GetMaterialApp(
+      theme: MAppTheme.LightTheme,
+      darkTheme: MAppTheme.DarkTheme,
+      themeMode: ThemeMode.system,
       home: const SplashScreen(),
     );
   }
